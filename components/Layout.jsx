a72d82a96aece4a0dd3b4bb8df0095ae91874f74
David Golden <david@soprisapps.com>
2021-11-11 11:45:37 -0500

    customize a bit :)

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,27 @@
+import {useState, useEffect} from 'react';
 import Head from 'next/head'
-import Header from './Header'
+import styles from "../styles/index.module.scss";
 
 export default function Layout({children, pageTitle, ...htmlPageProps}) {
+    const [checked, setChecked] = useState(null);
+
+    useEffect(() => {
+        if (typeof checked === "boolean") {
+            console.log("Did you think that was going to do something?");
+        }
+    }, [checked]);
+
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="shortcut icon" href={"/favicon.png"} type="image/x-icon" />
                 <title>{pageTitle}</title>
             </Head>
-            <section className="layout">
-                {/*<Header/>*/}
+            <section className={styles.main}>
+                <div className={`${styles.switch} ${checked ? styles.on : ""}`} onClick={() => setChecked(v => !v)}>
+                    <div className={styles.toggle} />
+                </div>
                 <div className="content" {...htmlPageProps}>{children}</div>
             </section>
         </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,16 +3,8 @@ import styles from '../styles/index.module.scss';
 
 const Index = () => {
     return <Layout className={styles.main} pageTitle={"David Golden"}>
-        <h1>David Golden</h1>
-        <h2>Senior Web Engineer
-            in Durango, CO</h2>
-        <main>
-            <p>
-                Welcome to my landing page! I'm a senior web engineer at SchoolBlocks. I try to spend as much time
-                outdoors (in the summer) as possible,
-                love long runs in the mountains, and my current obsession is beer and homebrewing.
-            </p>
-        </main>
+        <h1>A Landing Page for David Golden</h1>
+        <h2>Nothing to see here...</h2>
     </Layout>
 };
 
